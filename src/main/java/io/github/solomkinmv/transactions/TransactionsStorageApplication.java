a79d7f5a0e7e05ae2f1c51b043724e395f984134
msolomkin <msolomkin@playtika.com>
2021-09-22 00:10:30 +0300

    Initialize Spring project

---
+++ src/main/java/io/github/solomkinmv/transactions/TransactionsStorageApplication.java
@@ -0,0 +1,13 @@
+package io.github.solomkinmv.transactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionsStorageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionsStorageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/solomkinmv/transactions/TransactionsStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.solomkinmv.transactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionsStorageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
